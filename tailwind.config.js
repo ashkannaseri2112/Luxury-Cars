6e426158eb12ad51bb86e5e84708b762ea8a6b7e
ashkannaseri <ashkannaseri224@gmail.com>
2024-10-23 21:12:00 +0330

    update Configue File

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,26 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      container: true,
+      padding: {
+        DEFAULT: "1.5rem",
+        sm:"1rem",
+        lg:"1.25rem"
+      }
+    },
+    screens:{
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1320px",
+      },
   },
   plugins: [
-    
+    function ({ addVariant }) {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+    },
   ],
 }
 
