cf0ab3628ac2e22e093755c18fdc7a0a786a0a35
ashkannaseri <ashkannaseri224@gmail.com>
2024-10-24 16:04:37 +0330

    compelete Add Feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,33 +3,46 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      container: true,
-      padding: {
-        DEFAULT: "1.5rem",
-        sm:"1rem",
-        lg:"1.25rem"
+      colors:{
+        blue:{
+          850:"#21408E"
+        },
+        yellow:{
+           850:"#F1BC00"
+        },
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1.5rem",
+          sm: "1rem",
+          lg: "1.25rem",
+        },
       },
       fontSize: {
-        h2:"45px",
-        h3:"39px",
-        h4:"28px",
-        a:"22px",
-        p:"20px",
+        h2: "45px",
+        h3: "39px",
+        h4: "28px",
+        a: "22px",
+        p: "20px",
       },
-      fontFamily:{
-        Helvetica:"Helvetica",
-        HelveticaBold : "Helvetica-Bold"
-      }
+      fontFamily: {
+        Helvetica: "Helvetica",
+        HelveticaBold: "Helvetica-Bold",
+        HelveticaLight: "Helvetica-Light",
+        Roboto: "Roboto",
+        RobotoRegular: "Roboto-Regular",
+      },
     },
-    screens:{
+    screens: {
       sm: "640px",
       md: "768px",
       lg: "1024px",
       xl: "1320px",
-      },
+    },
   },
   plugins: [
-    function({addBase,theme}){
+    function ({ addBase, theme }) {
       addBase({
         h2: { fontSize: theme("fontSize.h2") },
         h3: { fontSize: theme("fontSize.h3") },
@@ -43,5 +56,4 @@ module.exports = {
       addVariant("child-hover", "& > *:hover");
     },
   ],
-}
-
+};
