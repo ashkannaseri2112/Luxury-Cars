e59b30019b79837889c0024d599534560a4b2ecd
ashkannaseri <ashkannaseri224@gmail.com>
2024-10-27 19:19:33 +0330

    update JavaScript Func

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,6 @@
 let $ = document
 let headerItems = $.querySelectorAll('.header-Item')
+let navListItems = $.querySelectorAll('.navList_Item')
 
 headerItems.forEach((item) => {
   item.addEventListener('mouseenter',() => {
@@ -9,6 +10,17 @@ headerItems.forEach((item) => {
     item.classList.remove('active')
   })
 })
+
+
+navListItems.forEach((listItem) => {
+  listItem.addEventListener('mouseenter',(event)=>{
+   event.target.classList.add('active')
+  })
+  listItem.addEventListener('mouseleave',(event)=> {
+    event.target.classList.remove('active')
+  })
+})
+
 
 function _id(element){
     $.getElementById(element)
