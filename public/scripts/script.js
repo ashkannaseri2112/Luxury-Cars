464bf02e5b0af8cb66c3a1d5b1bb3a4780a595e1
ashkannaseri <ashkannaseri224@gmail.com>
2024-10-28 13:51:19 +0330

    compelete  section Header

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,9 @@
 let $ = document
 let headerItems = $.querySelectorAll('.header-Item')
 let navListItems = $.querySelectorAll('.navList_Item')
+let menuBtn = $.getElementById('menu-btn')
+let navBtn = $.getElementById('nav-Btn')
+let navMenu = $.getElementById('nav-Menu')
 
 headerItems.forEach((item) => {
   item.addEventListener('mouseenter',() => {
@@ -29,3 +32,19 @@ function _id(element){
 function _selector(element){
     $.querySelector(element)
 }
+
+function openNav(){
+  navBtn.classList.add('active')
+  navMenu.classList.remove('-left-[280px]')
+  navMenu.classList.add('left-0')
+  overlay.classList.remove('hidden')
+}
+
+function closeNavBtn(){
+  navBtn.classList.remove('active')
+  navMenu.classList.add('-left-[280px]')
+  navMenu.classList.remove('left-0')
+  overlay.classList.add('hidden')
+}
+
+menuBtn.addEventListener('click',openNav)
